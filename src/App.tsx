79b036178d2772436e23e677fe6f8f3bc6f404f5
Alfredo Utria <alfredoutria@hotmail.com>
2023-05-22 12:39:05 -0500

    actualizando el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Principal from './Principal';
+import Contenido from './Contenido';
+import Listado from './Listado';
+import Agregar from './Agregar';
+import Actualizar from './Actualizar';
+import Eliminar from './Eliminar';
+import Nosotros from './Nosotros';
 
 function App() {
 
   return (
-    <>
-    
-      <Principal />
-      
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Principal />} />
+          <Route path='/contenido/:id' element={<Contenido />} />
+          <Route path='/listado' element={<Listado />} />
+          <Route path='/agregar' element={<Agregar />} />
+          <Route path='/actualizar/:id' element={<Actualizar />} />
+          <Route path='/eliminar/:id' element={<Eliminar />} />
+          <Route path='/nosotros' element={<Nosotros />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 
 }
 
 export default App;
+
