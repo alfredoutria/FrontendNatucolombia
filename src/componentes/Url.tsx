23a0214845f31db65c58972bea8ef322cf43c19f
Alfredo Utria <alfredoutria@hotmail.com>
2023-05-22 15:42:00 -0500

    actualizando el proyecto

--- src/componentes/Url.tsx
+++ src/componentes/Url.tsx
@@ -1,12 +1,12 @@
 
 const Url = {
 
-  agregar:'http://localhost:5000/api/agregar',
-  lista:'http://localhost:5000/api/lista',
-  listaId:'http://localhost:5000/api/obtenerdatosId',
-  actualizar:'http://localhost:5000/api/actualizar',
-  eliminar:'http://localhost:5000/api/eliminar',
-  comentario:'http://localhost:5000/api/comentario'
+  agregar: 'https://backenturismo.onrender.com/api/agregar',
+  lista: 'https://backenturismo.onrender.com/api/lista',
+  listaId: 'https://backenturismo.onrender.com/api/obtenerdatosId',
+  actualizar: 'https://backenturismo.onrender.com/api/actualizar',
+  eliminar: 'https://backenturismo.onrender.com/api/eliminar',
+  comentario: 'https://backenturismo.onrender.com/api/comentario'
 
 
 
@@ -18,14 +18,15 @@ export default Url;
 
   const Url ={
 
+  agregar:'http://localhost:5000/api/agregar',
+  lista:'http://localhost:5000/api/lista',
+  listaId:'http://localhost:5000/api/obtenerdatosId',
+  actualizar:'http://localhost:5000/api/actualizar',
+  eliminar:'http://localhost:5000/api/eliminar',
+  comentario:'http://localhost:5000/api/comentario'
   
 
-  agregar: 'https://backenturismo.onrender.com/api/agregar',
-  lista: 'https://backenturismo.onrender.com/api/lista',
-  listaId: 'https://backenturismo.onrender.com/api/obtenerdatosId',
-  actualizar: 'https://backenturismo.onrender.com/api/actualizar',
-  eliminar: 'https://backenturismo.onrender.com/api/eliminar',
-  comentario: 'https://backenturismo.onrender.com/api/comentario'
+ 
 
 
    
